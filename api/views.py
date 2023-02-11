921d2e52753357a81e84aae00ca499244ef75377
Garrett Edel <edelgm6@gmail.com>
2023-02-11 15:48:10 -0500

    Added authentication and a transaction endpoint

--- api/views.py
+++ api/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from api.serializers import TransactionOutputSerializer, JournalEntryInputSerializer, JournalEntryOutputSerializer
+from api.models import Transaction
 from api.forms import TransactionsUploadForm
 from api.CsvHandler import CsvHandler
 
-
+@method_decorator(login_required, name='dispatch')
 class Index(View):
     template = 'api/index.html'
     form = TransactionsUploadForm
@@ -28,41 +33,27 @@ class Index(View):
             print('invalid')
             return render(request, self.template, {'form': self.form})
 
+class TransactionView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-class TransactionsUploadViewChase(APIView):
-    parser_class = (FileUploadParser,)
+    def get(self, request, *args, **kwargs):
+        transactions = Transaction.objects.all()
+        transactions_output_serializer = TransactionOutputSerializer(transactions,many=True)
+        return Response(transactions_output_serializer.data)
 
-    def post(self, request, *args, **kwargs):
-        print(request.data)
-        transactions_serializer = ChaseCsvSerializer(data=request.data, many=True)
-        if transactions_serializer.is_valid():
-            print(valid)
-            transactions = transactions_serializer.save()
-            transactions_output_serializer = TransactionOutputSerializer(transactions,many=True)
-            return Response(transactions_output_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print(transactions_serializer.errors)
-            return Response(transactions_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class TransactionsUploadView(APIView):
-    parser_class = (FileUploadParser,)
-
-    def post(self, request, *args, **kwargs):
-        print(request.data)
-        file_serializer = TransactionCsvSerializer(data=request.data)
-        if file_serializer.is_valid():
-            files = file_serializer.save()
-            transactions_output_serializer = TransactionOutputSerializer(files,many=True)
-            return Response(transactions_output_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print(file_serializer.errors)
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class JournalEntryView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    # def get(self, request, *args, **kwargs):
+    #     journal_entries = JournalEntry.objects.all()
+    #     print(journal_entries)
+    #     journal_entry_output_serializer = JournalEntryOutputSerializer(journal_entries,many=True)
+    #     return Response(journal_entry_output_serializer.data)
 
     def post(self, request, *args, **kwargs):
-
-        print(request.data)
 
         journal_entry_input_serializer = JournalEntryInputSerializer(data=request.data)
 
@@ -73,3 +64,17 @@ class JournalEntryView(APIView):
         else:
             print(journal_entry_input_serializer.errors)
             return Response(journal_entry_input_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class TransactionsUploadView(APIView):
+#     parser_class = (FileUploadParser,)
+
+#     def post(self, request, *args, **kwargs):
+#         print(request.data)
+#         file_serializer = TransactionCsvSerializer(data=request.data)
+#         if file_serializer.is_valid():
+#             files = file_serializer.save()
+#             transactions_output_serializer = TransactionOutputSerializer(files,many=True)
+#             return Response(transactions_output_serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             print(file_serializer.errors)
+#             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
