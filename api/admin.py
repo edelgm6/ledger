b0595fa662bcfc94eed2385c2c8f15aa4e29b51f
Garrett Edel <edelgm6@gmail.com>
2023-02-15 08:32:26 -0500

    Added CSVProfile and added to the Account endpoint return

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from api.models import Account, Transaction, JournalEntry, JournalEntryItem, AutoTag
+from api.models import Account, Transaction, JournalEntry, JournalEntryItem, AutoTag, CSVProfile
 
 admin.site.register(Account)
 admin.site.register(Transaction)
 admin.site.register(JournalEntry)
 admin.site.register(JournalEntryItem)
 admin.site.register(AutoTag)
+admin.site.register(CSVProfile)
