1e46e4b7e4c9f0d9ec0add7dd10c50c95d042081
Garrett Edel <edelgm6@gmail.com>
2023-03-02 17:53:17 -0500

    Added templates

--- ledger/settings.py
+++ ledger/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'rest_framework',
     'api',
     'rest_framework.authtoken',
