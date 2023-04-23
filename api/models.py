bc0ec10c1a241f14957d3b20f5b6494c36edb4da
Garrett Edel <edelgm6@gmail.com>
2023-04-23 15:40:54 -0400

    Removed concept of suggested accounts

--- api/models.py
+++ api/models.py
@@ -70,9 +70,7 @@ class Transaction(models.Model):
     category = models.CharField(max_length=200,blank=True)
     is_closed = models.BooleanField(default=False)
     date_closed = models.DateField(null=True,blank=True)
-    suggested_account = models.ForeignKey('Account',related_name='suggested_account',on_delete=models.CASCADE,null=True,blank=True)
     type = models.CharField(max_length=25,choices=TransactionType.choices,blank=True)
-    suggested_type = models.CharField(max_length=25,choices=TransactionType.choices,blank=True)
     linked_transaction = models.OneToOneField('Transaction',on_delete=models.SET_NULL,null=True,blank=True)
 
     def __str__(self):
