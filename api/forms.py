febb3db95ea27ccc85bd486d3f7cee02b72fb438
Garrett Edel <edelgm6@gmail.com>
2023-11-12 11:06:52 -0500

    Added suggested account to the form

--- api/forms.py
+++ api/forms.py
@@ -6,11 +6,14 @@ from api.models import Transaction, Account
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = ['date','amount','description']
+        fields = ['date','amount','description','suggested_account']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
             'amount': forms.NumberInput(attrs={'step': '1'})
         }
+        suggested_account = forms.ModelChoiceField(
+            queryset=Account.objects.all()
+        )
 
     def __init__(self, *args, **kwargs):
         super(TransactionForm, self).__init__(*args, **kwargs)
@@ -18,11 +21,11 @@ class TransactionForm(forms.ModelForm):
 
     def save(self, *args, **kwargs):
         instance = super(TransactionForm, self).save(commit=False)
-
         wallet = Account.objects.get(special_type=Account.SpecialType.WALLET)
 
         instance.account = wallet
         instance.amount = instance.amount * -1
-
+        print(instance.suggested_account)
         instance.save()
+        print(instance.suggested_account)
         return instance
