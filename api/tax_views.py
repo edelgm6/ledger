b79b84b8a7bd1d27b2e267b68a2e56d0cf16bb26
Garrett Edel <edelgm6@gmail.com>
2023-12-17 10:23:39 -0500

    Finalized taxes view

--- api/tax_views.py
+++ api/tax_views.py
@@ -38,8 +38,18 @@ class TaxChargeMixIn:
         if current_taxable_income:
             tax_charge.current_tax = tax_charge.tax_rate * current_taxable_income
 
+    def get_tax_filter_form_html(self):
+        filter_template = 'api/filter_forms/tax-charge-filter-form.html'
+        html = render_to_string(filter_template, {'filter_form': TaxChargeFilterForm()})
+        return html
 
-    def get_tax_form_html(self, last_day_of_month=None):
+    def get_tax_form_html(self, tax_charge=None, last_day_of_month=None):
+        if tax_charge:
+            form = TaxChargeForm(instance=tax_charge)
+            last_day_of_month = tax_charge.date
+        else:
+            form = TaxChargeForm()
+
         last_day_of_month = last_day_of_month if last_day_of_month else self._get_last_day_of_last_month()
         first_day_of_month = date(last_day_of_month.year, last_day_of_month.month, 1)
         income_statement = IncomeStatement(last_day_of_month, first_day_of_month)
@@ -49,19 +59,19 @@ class TaxChargeMixIn:
 
         current_taxable_income = income_statement.get_taxable_income()
 
-        for tax_charge in [latest_federal_tax_charge,latest_state_tax_charge]:
-            if tax_charge:
-                self._add_tax_rate_and_charge(tax_charge, current_taxable_income)
+        for latest_tax_charge in [latest_federal_tax_charge,latest_state_tax_charge]:
+            if latest_tax_charge:
+                self._add_tax_rate_and_charge(latest_tax_charge, current_taxable_income)
 
-        form_template = 'api/entry_forms/edit-tax-charge-form.html'
         context = {
-            'form': TaxChargeForm(),
+            'form': form,
             'taxable_income': current_taxable_income,
             'latest_federal_tax_charge': latest_federal_tax_charge,
             'latest_state_tax_charge': latest_state_tax_charge,
-            'latest_property_tax_charge': latest_property_tax_charge
+            'latest_property_tax_charge': latest_property_tax_charge,
+            'tax_charge': tax_charge
         }
-
+        form_template = 'api/entry_forms/edit-tax-charge-form.html'
         form_html = render_to_string(form_template, context)
         return form_html
 
@@ -89,34 +99,27 @@ class TaxChargeTableView(TaxChargeMixIn, LoginRequiredMixin, View):
             tax_table_charge_table_html = self.get_tax_table_html(tax_charges)
 
             template = 'api/components/taxes-content.html'
-            form_template = 'api/entry_forms/edit-tax-charge-form.html'
             context = {
                 'tax_charge_table': tax_table_charge_table_html,
                 'form': self.get_tax_form_html(),
             }
+            html = render_to_string(template, context)
 
-            return render(request, template, context)
+            return HttpResponse(html)
 
 class TaxChargeFormView(TaxChargeMixIn, LoginRequiredMixin, View):
     login_url = '/login/'
     redirect_field_name = 'next'
-    form_class = TaxChargeForm
-    form_template = 'api/entry_forms/edit-tax-charge-form.html'
 
     def get(self, request, pk=None, *args, **kwargs):
         if pk:
             tax_charge = get_object_or_404(TaxCharge, pk=pk)
-            form = self.form_class(instance=tax_charge)
         else:
             tax_charge = None
-            form = self.form_class()
 
-        context = {
-            'form': form,
-            'tax_charge': tax_charge
-        }
+        form_html = self.get_tax_form_html(tax_charge=tax_charge)
 
-        return render(request, self.form_template, context)
+        return HttpResponse(form_html)
 
 # Loads full page
 class TaxesView(TaxChargeMixIn, LoginRequiredMixin, View):
@@ -126,23 +129,24 @@ class TaxesView(TaxChargeMixIn, LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
 
         tax_charges = TaxCharge.objects.filter(date__gte='2023-01-31',date__lte=self._get_last_day_of_last_month())
-        tax_charge_table = self.get_tax_table_html(tax_charges)
-        template = 'api/views/taxes.html'
-        filter_template = 'api/filter_forms/tax-charge-filter-form.html'
-        context = {
-            'tax_charge_table': tax_charge_table,
-            'form': self.get_tax_form_html(self._get_last_day_of_last_month()),
-            'filter_form': render_to_string(filter_template, {'filter_form': TaxChargeFilterForm()})
-        }
 
-        return render(request, template, context)
+        context = {
+            'tax_charge_table': self.get_tax_table_html(tax_charges),
+            'form': self.get_tax_form_html(last_day_of_month=self._get_last_day_of_last_month()),
+            'filter_form': self.get_tax_filter_form_html
+        }
+        html = render_to_string(template, context)
+
+        template = 'api/views/taxes.html'
+        return HttpResponse(html)
 
     def post(self, request, pk=None, *args, **kwargs):
+        form_class = TaxChargeForm
         if pk:
             tax_charge = get_object_or_404(TaxCharge, pk=pk)
-            form = self.form_class(data=request.POST, instance=tax_charge)
+            form = form_class(data=request.POST, instance=tax_charge)
         else:
-            form = self.form_class(data=request.POST)
+            form = form_class(data=request.POST)
 
         if form.is_valid():
             tax_charge = form.save()
@@ -154,5 +158,6 @@ class TaxesView(TaxChargeMixIn, LoginRequiredMixin, View):
                 'tax_charge_table': self.get_tax_table_html(tax_charges),
                 'form': self.get_tax_form_html()
             }
-            form_template = 'api/components/taxes-content.html'
-            return render(request, form_template, context)
+            template = 'api/components/taxes-content.html'
+            html = render_to_string(template, context)
+            return HttpResponse(html)
