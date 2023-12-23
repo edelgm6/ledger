7d9772e1f06703453c2ad1bc34a99207c78b2263
Garrett Edel <edelgm6@gmail.com>
2023-12-23 17:17:43 -0500

    Updated Linking page, still not exactly right on highlighting

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView
 from api.views.frontend_views import TrendView,  UploadTransactionsView, IndexView
-from api.views.transaction_views import TransactionContentView, TransactionFormView, JournalEntryView, LinkTransactionsView, JournalEntryFormView, JournalEntryTableView, TransactionsView
+from api.views.transaction_views import LinkTransactionsContentView, TransactionContentView, TransactionFormView, JournalEntryView, LinkTransactionsView, JournalEntryFormView, JournalEntryTableView, TransactionsView
 from api.views.tax_views import TaxChargeTableView,TaxChargeFormView, TaxesView
 from api.views.reconciliation_views import ReconciliationTableView, ReconciliationView
 from api.views.amortization_views import AmortizationFormView, AmortizationView, AmortizeFormView
@@ -22,6 +22,7 @@ urlpatterns = [
 
     # Link page
     path('transactions-linking/', LinkTransactionsView.as_view(), name='link-transactions'),
+    path('transactions-linking/content', LinkTransactionsContentView.as_view(), name='link-transactions-content'),
 
     # Transactions page
     path('transactions/', TransactionsView.as_view(), name='transactions'),
