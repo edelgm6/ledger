1c6986eacdf73d367c4443aee8af644f4c6f62e6
Garrett Edel <edelgm6@gmail.com>
2023-12-26 09:07:43 -0500

    Dead code removal

--- api/forms.py
+++ api/forms.py
@@ -78,21 +78,6 @@ class ReconciliationFilterForm(forms.Form):
         reconciliations = ReconciliationFactory.create_bulk_reconciliations(date=create_date)
         return reconciliations
 
-    #     existing_reconciliations = set(Reconciliation.objects.filter(date=create_date).values_list('account__name', flat=True))
-    #     balance_sheet_account_names = set(Account.objects.filter(type__in=[Account.Type.ASSET, Account.Type.LIABILITY]).values_list('name', flat=True))
-
-    #     new_reconciliations = balance_sheet_account_names - existing_reconciliations
-    #     new_reconciliation_list = [
-    #         Reconciliation(account=Account.objects.get(name=account_name), date=create_date)
-    #         for account_name in new_reconciliations
-    #     ]
-
-    #     if new_reconciliation_list:
-    #         Reconciliation.objects.bulk_create(new_reconciliation_list)
-
-    #     reconciliations = Reconciliation.objects.filter(date=create_date)
-    #     return reconciliations
-
 class ReconciliationForm(forms.ModelForm):
     class Meta:
         model = Reconciliation
