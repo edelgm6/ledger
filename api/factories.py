8f6573aed1759985254528b2714dc84e32f2ba39
Garrett Edel <edelgm6@gmail.com>
2024-01-06 13:52:35 -0500

    Added closable accounts and excluded from key pages

--- api/factories.py
+++ api/factories.py
@@ -5,7 +5,10 @@ class ReconciliationFactory:
     @staticmethod
     def create_bulk_reconciliations(date):
         existing_reconciliations = set(Reconciliation.objects.filter(date=date).values_list('account__name', flat=True))
-        balance_sheet_account_names = set(Account.objects.filter(type__in=[Account.Type.ASSET, Account.Type.LIABILITY]).values_list('name', flat=True))
+        balance_sheet_account_names = set(Account.objects.filter(
+            type__in=[Account.Type.ASSET, Account.Type.LIABILITY],
+            is_closed=False
+        ).values_list('name', flat=True))
 
         new_reconciliations = balance_sheet_account_names - existing_reconciliations
         new_reconciliation_list = [
