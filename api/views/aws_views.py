15589ddba9fe54fa72af1fea4d6bd3b0b71c41e3
Garrett Edel <edelgm6@gmail.com>
2024-07-08 14:57:30 -0400

    Created textract job

--- api/views/aws_views.py
+++ api/views/aws_views.py
@@ -9,11 +9,10 @@ def upload_document(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            document = request.FILES['document']
-            s3_response = upload_to_s3(document)
-            print(s3_response)
-            # response = process_document_with_textract(document)
-            return JsonResponse(s3_response)
+            # document = request.FILES['document']
+            # response = upload_to_s3(document)
+            response = process_document_with_textract('block pay.pdf')
+            return JsonResponse(response)
     else:
         form = DocumentForm()
     return render(request, 'upload.html', {'form': form})
@@ -37,7 +36,7 @@ def upload_to_s3(file):
     except Exception as e:
         return {'error': str(e), 'message': 'Upload failed'}
 
-def process_document_with_textract(document):
+def process_document_with_textract(document_name):
     # Boto3 client for Textract
     client = boto3.client(
         'textract',
@@ -47,8 +46,17 @@ def process_document_with_textract(document):
     )
 
     # Upload the document to S3 or process it directly
-    response = client.start_document_text_detection(
-        Document={'Bytes': document.read()}
+    response = client.start_document_analysis(
+        DocumentLocation={
+            'S3Object': {
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                'Name': document_name
+            }
+        },
+        FeatureTypes=[
+            'TABLES','FORMS'
+        ]
     )
+    print(response)
     
     return response
