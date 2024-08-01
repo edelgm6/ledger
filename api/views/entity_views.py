19310d07ba2e56fcd69dd27163bddec5b7197184
Garrett Edel <edelgm6@gmail.com>
2024-08-01 06:51:01 -0400

    Added entity balances list

--- api/views/entity_views.py
+++ api/views/entity_views.py
@@ -3,12 +3,33 @@ from django.template.loader import render_to_string
 from django.views import View
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
-from api.models import Account, JournalEntryItem, Entity
+from django.db.models import Sum, Case, When, F, Value, DecimalField
+from django.db.models.functions import Round
+from api.models import Account, JournalEntryItem
 from api.forms import JournalEntryItemEntityForm
 
 # TODO: Create a mixin to handle common logic
 class EntityTagMixin:
+
+    def get_entities_balances(self):
+        entities_balances = JournalEntryItem.objects.exclude(entity__isnull=True).values('entity__id', 'entity__name').annotate(
+            total_debits=Sum(
+                Case(
+                    When(type=JournalEntryItem.JournalEntryType.DEBIT, then=F('amount')),
+                    default=Value(0),
+                    output_field=DecimalField()
+                )
+            ),
+            total_credits=Sum(
+                Case(
+                    When(type=JournalEntryItem.JournalEntryType.CREDIT, then=F('amount')),
+                    default=Value(0),
+                    output_field=DecimalField()
+                )
+            ),
+            balance=F('total_debits') - F('total_credits')
+        )
+        return entities_balances
 
     def get_total_page_html(self, is_initial_load=False):
         # Need to create a new account sub type for payables
@@ -22,8 +43,6 @@ class EntityTagMixin:
             account__sub_type__in=relevant_account_types
         ).select_related('journal_entry__transaction')
 
-        # entity_balances = Entity.objects.all().prefetch_related('journal_entry_items')
-
         table_html = render_to_string('api/tables/payables-receivables-table.html', {'payables_receivables': untagged_journal_entry_items})
         try:
             initial_journal_entry_item = untagged_journal_entry_items[0]
@@ -36,13 +55,20 @@ class EntityTagMixin:
                 'journal_entry_item_id': initial_journal_entry_item.pk
             }
         )
+        entity_balances_table_html = render_to_string(
+            'api/tables/entity-balances-table.html',
+            {
+                'entities_balances': self.get_entities_balances()
+            }
+        )
 
         html = render_to_string(
             'api/views/payables-receivables.html', 
             {
                 'table': table_html,
                 'form': form_html,
-                'is_initial_load': is_initial_load
+                'is_initial_load': is_initial_load,
+                'balances_table': entity_balances_table_html
             }
         )
         return html
@@ -79,5 +105,6 @@ class TagEntitiesView(LoginRequiredMixin, EntityTagMixin, View):
 
     def get(self, request):
         html = self.get_total_page_html(is_initial_load=True)
+        self.get_entities_balances()
 
         return HttpResponse(html)
