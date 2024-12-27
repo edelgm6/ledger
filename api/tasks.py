ae56d94dbb947359afebf5a9966c9afd82735894
Garrett Edel <edelgm6@gmail.com>
2024-12-27 17:08:37 -0500

    Cleanup

--- api/tasks.py
+++ api/tasks.py
@@ -10,8 +10,3 @@ def orchestrate_paystub_extraction(s3file_pk):
     job_id = s3file.create_textract_job()
     wait_for_textract_completion(job_id)
     s3file.create_paystubs_from_textract_data()
-
-
-@shared_task
-def add(x, y):
-    return x + y
