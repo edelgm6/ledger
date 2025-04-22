f9a649013f53b19b495a9a4a2b225f46b187bafd
Garrett Edel <edelgm6@gmail.com>
2025-04-22 06:35:49 -0700

    Cleanup

--- api/views/journal_entry_views_alt.py
+++ api/views/journal_entry_views_alt.py
@@ -1,9 +1,6 @@
-import json
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from django.urls import reverse
 from django.views import View
 
 from api.models import Transaction
