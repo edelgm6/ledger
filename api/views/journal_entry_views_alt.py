2d3578f2e4dc6022325a2891aaf92c717e710029
Garrett Edel <edelgm6@gmail.com>
2025-05-26 21:22:19 -0400

    Rejiggered vscode extensions

--- api/views/journal_entry_views_alt.py
+++ api/views/journal_entry_views_alt.py
@@ -9,6 +9,7 @@ from api.services.journal_entry_services import (
     get_journal_entry_form_html,
     get_transaction_store_to_html,
 )
+
 from api.views.mixins import JournalEntryViewMixin
 from api.views.transaction_views import TransactionsViewMixin
 
@@ -23,13 +24,12 @@ transaction to preload
 
 
 class JournalEntryUpdate(View):
-    
     # TODO: load the form plus button
     def post(self, request, transaction_id):
-        print('journalentryupdate')
+        print("journalentryupdate")
         # TODO: Process the post
 
-        transaction_ids = request.POST.get('transaction_ids')
+        transaction_ids = request.POST.get("transaction_ids")
         transaction_ids = convert_frontend_list_to_python(frontend_list=transaction_ids)
         transaction = Transaction.objects.get(pk=transaction_id)
         transaction_index = transaction_ids.index(transaction_id)
@@ -39,34 +39,45 @@ class JournalEntryUpdate(View):
         print(transaction_index)
         print(len(transaction_ids))
         if len(transaction_ids) == 1:
-            return HttpResponse('')
+            return HttpResponse("")
         elif transaction_index < len(transaction_ids) - 1:
-            next_transaction = Transaction.objects.get(pk=transaction_ids[transaction_index + 1])
+            next_transaction = Transaction.objects.get(
+                pk=transaction_ids[transaction_index + 1]
+            )
         else:
-            next_transaction = Transaction.objects.get(pk=transaction_ids[transaction_index - 1])
+            next_transaction = Transaction.objects.get(
+                pk=transaction_ids[transaction_index - 1]
+            )
         transaction_ids.remove(transaction_id)
         print(transaction_ids)
-        html = get_journal_entry_form_html(transaction=next_transaction, transaction_ids=transaction_ids)
+        html = get_journal_entry_form_html(
+            transaction=next_transaction, transaction_ids=transaction_ids
+        )
 
-        transaction_store_html = get_transaction_store_to_html(transaction_ids=transaction_ids, swap_oob=True)
+        transaction_store_html = get_transaction_store_to_html(
+            transaction_ids=transaction_ids, swap_oob=True
+        )
         html += transaction_store_html
         response = HttpResponse(html)
 
         return response
 
+
 class JournalEntryButton(View):
-    
-    def get(self, request, transaction_id):
-        print('journalentrybutton')
+    def get(self, request, transaction_id) -> Int:
+        print("journalentrybutton")
         if not transaction_id:
             return ""
-        
-        transaction_ids = request.GET.get('transaction_ids')
+
+        transaction_ids = request.GET.get("transaction_ids")
         transaction_ids = convert_frontend_list_to_python(frontend_list=transaction_ids)
 
         transaction = Transaction.objects.get(pk=transaction_id)
-        html = get_journal_entry_form_html(transaction=transaction, transaction_ids=transaction_ids)
+        html = get_journal_entry_form_html(
+            transaction=transaction, transaction_ids=transaction_ids
+        )
         return HttpResponse(html)
+
 
 # Called as the main page
 class JournalEntryViewAlt(
@@ -77,30 +88,30 @@ class JournalEntryViewAlt(
     view_template = "api/views/journal-entry-view-alt.html"
 
     def get_table_html(self, transactions):
-        print('get_table_html')
-        transaction_ids = transactions.values_list('id', flat=True)
-        context = {
-            "transactions": transactions,
-            "transaction_ids": transaction_ids
-        }
+        print("get_table_html")
+        transaction_ids = transactions.values_list("id", flat=True)
+        context = {"transactions": transactions, "transaction_ids": transaction_ids}
 
         table_template = "api/tables/transactions-table-alt.html"
         return render_to_string(table_template, context)
-    
+
     def get(self, request):
-        print('get')
-        transactions = Transaction.objects.all().order_by('date')
-        table_html = self.get_table_html(
-            transactions=transactions
+        print("get")
+        transactions = Transaction.objects.all().order_by("date")
+        table_html = self.get_table_html(transactions=transactions)
+        jei_form_html = get_journal_entry_form_html(
+            transaction=transactions[0],
+            transaction_ids=transactions.values_list("id", flat=True),
         )
-        jei_form_html = get_journal_entry_form_html(transaction=transactions[0], transaction_ids=transactions.values_list('id', flat=True))
-        transaction_ids = transactions.values_list('id', flat=True)
-        transaction_store_html = get_transaction_store_to_html(transaction_ids=transaction_ids)
+        transaction_ids = transactions.values_list("id", flat=True)
+        transaction_store_html = get_transaction_store_to_html(
+            transaction_ids=transaction_ids
+        )
         context = {
             "table": table_html,
             "form": jei_form_html,
             "transaction_ids": transaction_ids,
-            "transaction_store": transaction_store_html
+            "transaction_store": transaction_store_html,
         }
 
         html = render_to_string(self.view_template, context)
